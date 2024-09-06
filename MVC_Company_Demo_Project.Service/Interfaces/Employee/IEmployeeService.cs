233125966d98f89ba00e1f036c659c594f913f91
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-06 11:07:43 +0300

    Create Employee Services

---
+++ MVC_Company_Demo_Project.Service/Interfaces/Employee/IEmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using MVC_Company_Demo_Project.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Company_Demo_Project.Service.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Employee GetById(int? id);
+
+        IEnumerable<Employee> GetAll();
+
+        void Add(Employee department);
+
+        void Update(Employee department);
+
+        void Delete(Employee department);
+
+    }
+}
