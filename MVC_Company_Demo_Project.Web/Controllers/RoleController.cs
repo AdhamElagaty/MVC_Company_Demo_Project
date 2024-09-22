1dda07c844652f1975cb7a8dd466101ee6d1fb7c
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-22 05:29:48 +0300

    RolesController(Index & Create)

---
+++ MVC_Company_Demo_Project.Web/Controllers/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Company_Demo_Project.Data.Models;
+using MVC_Company_Demo_Project.Web.Models;
+
+namespace MVC_Company_Demo_Project.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RoleController> _logger;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, ILogger<RoleController> logger)
+        {
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleViewModel roleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole()
+                {
+                    Name = roleModel.Name
+                };
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                foreach (var error in result.Errors)
+                    _logger.LogError(error.Description);
+            }
+            return View(roleModel);
+        }
+    }
+}
---
+++ MVC_Company_Demo_Project.Web/Models/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Company_Demo_Project.Web.Models
+{
+    public class RoleViewModel
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
